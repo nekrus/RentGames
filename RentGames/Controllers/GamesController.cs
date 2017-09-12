7c5a8309a854190238ec513fd961e1931650fcca
Damian Pawłowski <dam.pawlowski@gmail.com>
2017-09-12 09:13:50 +0200

    First commit. Add a project to the repository.

---
+++ RentGames/Controllers/GamesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentGames.Models;
+
+namespace RentGames.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Random()
+        {
+            var game = new Game() { Name = "Diablo" };
+
+            ViewData["Game"] = game;
+            return View(game);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("games/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ RentGames/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentGames.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
